4f91da9aca903350bc40d4d8b17ed19361c45743
xiaowu20121212 <1078336055@qq.com>
2018-07-16 21:35:36 +0800

    完善代码

--- app/src/main/java/com/example/yuanzhiwu/memorydemo/MainActivity.java
+++ app/src/main/java/com/example/yuanzhiwu/memorydemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.yuanzhiwu.memorydemo;
 
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button mSysTrace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSysTrace = findViewById(R.id.btn_sys_trace);
+        mSysTrace.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        if (mSysTrace == v) {
+            intent = new Intent(this, RecycleViewActivity.class);
+        }
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/yuanzhiwu/memorydemo/RecycleViewActivity.java
@@ -0,0 +1,79 @@
+package com.example.yuanzhiwu.memorydemo;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Trace;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecycleViewActivity extends Activity {
+    private RecyclerView mRecycleView;
+    TestAdapter mAdapter;
+    private EditText mInput;
+    private List<String> mData = new ArrayList<>();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycle_layout);
+        mRecycleView = findViewById(R.id.recycle_view);
+        mRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        for(int i = 0; i < 1000; i++) {
+            mData.add(String.valueOf(i));
+        }
+        mAdapter = new TestAdapter();
+        mRecycleView.setAdapter(mAdapter);
+        mInput = findViewById(R.id.ed_input);
+    }
+
+    private class TestAdapter extends RecyclerView.Adapter<TestViewHolder> {
+
+
+        @NonNull
+        @Override
+        public TestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new TestViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, null));
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+        @Override
+        public void onBindViewHolder(@NonNull TestViewHolder holder, int position) {
+            Trace.beginSection("onBindViewHolder");
+            holder.freshData(mData.get(position));
+            Trace.endSection();
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+    }
+
+    private class TestViewHolder extends RecyclerView.ViewHolder {
+
+        public TestViewHolder(View itemView) {
+            super(itemView);
+        }
+        void freshData(String s) {
+            int j = 0;
+            String input = mInput.getText().toString();
+            for(int i = 0; i < Math.random() * (TextUtils.isEmpty(input) ? 1: Integer.parseInt(input)) ; i++) {
+                j = i;
+            }
+            ((TextView) itemView.findViewById(R.id.tv_number)).setText(String.format("position: %s  random: %s", s, String.valueOf(j)));
+
+        }
+    }
+}
